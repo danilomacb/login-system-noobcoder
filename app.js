63ee0b84237248a3659f906e52cae09076ec4755
danilomacb <danilo.bakun@gmail.com>
2020-11-02 10:12:16 -0300

    MERN Stack Part 2 : Mongoose and Bcrypt

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ mongoose.connect(
   }
 );
 
+const User = require("./models/User");
+
+const userInput = {
+  username: "danilo",
+  password: "123",
+  role: "admin",
+};
+
+const user = new User(userInput);
+user.save((err, document) => {
+  if (err) console.log(err);
+
+  console.log(document);
+});
+
 app.listen(5000, () => {
   console.log("express server started");
 });
