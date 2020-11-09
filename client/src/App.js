56fef860e95d1081c982f78e0b8ca8cf00112c9b
danilomacb <danilo.bakun@gmail.com>
2020-11-09 09:11:42 -0300

    MERN Stack part 9: Protecting our Routes with HOCS PrivateRoute and UnPrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Login from "./Components/Login";
 import Register from "./Components/Register";
 import Home from "./Components/Home";
 import Todos from "./Components/Todos";
+import Admin from "./Components/Admin";
+import PrivateRoute from "./hocs/PrivateRoute";
+import UnPrivateRoute from "./hocs/UnPrivateRoute";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -11,9 +14,10 @@ function App() {
     <Router>
       <Navbar />
       <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/todos" component={Todos} />
+      <UnPrivateRoute path="/login" component={Login} />
+      <UnPrivateRoute path="/register" component={Register} />
+      <PrivateRoute path="/todos" roles={["user", "admin"]} component={Todos} />
+      <PrivateRoute path="/admin" roles={["admin"]} component={Admin} />
     </Router>
   );
 }
